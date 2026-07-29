6aa050f072d75e94f4c492f931cf0561c5eb614b
rlarldhks154 <rlarldhks154@gmail.com>
2026-07-29 15:37:48 +0900

    스트림릿 수업

---
+++ Streamlit/10_cache.py
@@ -0,0 +1,23 @@
+# ========================================================================
+# ~/bigdata2026/fastapi/Streamlit/10_cache.py
+#   
+#   캐시가 있을 때의 경우
+# ========================================================================
+import streamlit as st
+import pandas as pd
+import time
+
+
+@st.cache_data
+def load_subway_data():
+    time.sleep(2)
+    return pd.read_csv('subway_long.csv', encoding='utf-8', index_col=False)
+
+st.title('캐싱 적용해보기')
+
+station = st.selectbox('역을 선택하세요.', ['동대구역', '반월당역', '범어역'])
+
+# 역을 바꿀 때마다 매번 2초씩 다시 로딩
+df = load_subway_data()
+st.write(f'{station} 데이터 로딩 완료')
+st.dataframe(df.head())
